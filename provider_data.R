cc807aae916b5916f77252ef69b3eb6f517a027b
arosen52 <aaronrosenberger@gmail.com>
2014-07-21 10:02:59 -0700

    Works from saved data frame rather than SPSS file. Removed the require(memisc) command; MASS select conflicts with dplyr select.

--- provider_data.R
+++ provider_data.R
@@ -1,21 +1,16 @@
-require(memisc)
 require(knitr)
 require(reshape2)
 require(dplyr)
 require(rmarkdown)
 
-wrma <- as.data.set(spss.system.file("//Dolores/Active/Projects/WRMA_Funding_Streams/Phase_1/SurveyData/RW Funding Streams 5.sav"), 
-                    stringsAsFactors = FALSE)
-
-wrma_df <- data.frame(wrma, stringsAsFactors = FALSE)
-
+load("wrma_df.Rda")
 char <- wrma_df %>%
         mutate(pcmh = q7 == "Yes", 
                hh   = regexpr("^Yes", q8) > 0, 
                `MCO Participation` = "", 
                mcaid = !is.na(q11_1) | !is.na(q11_2) | !is.na(q11_3) | !is.na(q11_4), 
                priv  = ) %>%
-        dplyr::select(providerid, pcmh, hh, `MCO Participation`, mcaid) # MASS conflicts with dplyr select
+               select(providerid, pcmh, hh, `MCO Participation`, mcaid) 
 
 prov_char <- wrma_df %>%
              filter(providerid == 353) %>%
@@ -24,7 +19,7 @@ prov_char <- wrma_df %>%
                     `MCO Participation` = "", 
                     `  Medicaid` = ifelse(!is.na(q11_1) | !is.na(q11_2) | !is.na(q11_3) | !is.na(q11_4), "Yes", "No")) %>%
              melt(id.var = "providerid") %>%
-             dplyr::select(-providerid) %>%
+             select(-providerid) %>%
              inner_join(
                (char %>% 
                  summarize(`Patient centered medical home` = mean(pcmh), 
